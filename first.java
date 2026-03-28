c0ff415f9a768c1571666ca8b63ebe254010f95e
akshatabijapur5-hub <akshatabijapur5@gmail.com>
2026-03-28 15:30:07 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 public class first {
 
     public static void main (String[] args){
         System.out.println("hello Java");
+        System.out.println("welcome to gpt");
     }
 }
-=======
-public class first {
 
-    public static void main (String[] args){
-        System.out.println("hello Java");
-        System.out.println("welcome to gpt);
-    }
-}
->>>>>>> 485d9d07d0c3b3333bd54adaa89441d243c19a9f
